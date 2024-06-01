afced07b987ab715de018db46f2c8c8b175ffe18
bl1ndmole <ichetkinevgenii@gmail.com>
2024-06-01 14:09:40 +0800

    Update main (1).cpp

--- main (1).cpp
+++ main (1).cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    for (int i = 0; i <= n; i++) {
+    for (int i = 0; i < n; i++) {
         string a;
-        for (int j = 0; j < i; j++) a+='*';
+        for (int j = 0; j <= i; j++) a+='*';
         cout << a << endl;
     }
 }
